9f06903954770c7174756b4412d5b7d72b963e64
raissabispo <uamr.ieadpe1>
2024-07-24 15:57:03 -0300

    Primeira aula

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("teste")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Olá"
+
+@app.route("/teste")
+def teste():
+    return "Página de teste"
+
+if __name__ == "__main__":
+    app.run()
