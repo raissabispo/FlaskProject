8707fb0352e3938810d3ef678e22dd74d72f1473
raissabispo <uamr.ieadpe1>
2024-07-24 21:31:50 -0300

    commit

--- src/app.py
+++ src/app.py
@@ -4,13 +4,14 @@ from controllers.employeeController import employee_blueprint
 
 app = Flask(__name__, template_folder="views")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init__app(app)
+db.init_app(app)
 
 app.register_blueprint(employee_blueprint, url_prefix='/')
 
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run()
