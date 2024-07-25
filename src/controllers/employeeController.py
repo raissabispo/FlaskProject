baa7e6f36dca1bcc53b863c93ff2e04503cf3f45
raissabispo <uamr.ieadpe1>
2024-07-25 20:32:14 -0300

    Finalizando o projeto

--- src/controllers/employeeController.py
+++ src/controllers/employeeController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, abort
 from models.employeeModel import EmployeeModel, db
  
 employee_blueprint = Blueprint('employee', __name__)
@@ -17,8 +17,7 @@ def create():
         cpf = request.form['cpf']
         name = request.form['name']
         position = request.form['position']
-        employee = EmployeeModel(cpf = cpf, name = name, position=position)
-       
+        employee = EmployeeModel(cpf = cpf, name = name, position=position) 
         db.session.add(employee)
         db.session.commit()
         return redirect('/data')
@@ -26,4 +25,39 @@ def create():
 @employee_blueprint.route('/data')
 def DataView():
     employee = EmployeeModel.query.all()
-    return render_template('datalist.html',employee=employee)
+    return render_template('datalist.html', employee=employee)
+
+@employee_blueprint.route('/data/<int:id>')
+def findEmployee(id):
+    # buscar uma id
+    employee = EmployeeModel.query.filter_by(id=id).first()
+    if employee:
+        return render_template("data.html", employee=employee)
+    else:
+        return f"Empregado com id={id} não existe"
+    
+@employee_blueprint.route('/data/<int:id>/update', methods=["GET","POST"])
+# editar o usuario
+def update(id):
+    employee = EmployeeModel.query.get(id)
+    if not employee:
+        return "Empregado com id={id} não existe"
+    if request.method == 'POST':
+        employee.cpf = request.form["cpf"]
+        employee.name = request.form["name"]
+        employee.position = request.form["position"]
+        db.session.commit()
+        return redirect(f"/data/{id}")
+    return render_template("update.html", employee=employee)
+
+@employee_blueprint.route('/data/<int:id>/delete', methods=["GET","POST"])
+# excluir o usuario
+def delete(id):
+    employee = EmployeeModel.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            return redirect('/data')
+        abort(404)
+    return render_template('delete.html', employee=employee)
